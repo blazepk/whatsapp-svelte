22805fb19c88b072c508aebff724369bc1f7f143
blazepk <pratyushkk880@gmail.com>
2023-07-30 14:41:09 +0530

    initial config

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import postcss from './postcss.config.js';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	css: {
+		postcss
+	}
+});
